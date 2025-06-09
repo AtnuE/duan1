ee8367d18fd49f6054d4369f304d5e86b31d2e70
AtnuE <161493031+AtnuE@users.noreply.github.com>
2025-06-09 20:25:54 +0700

    upload no1

---
+++ star.js
@@ -0,0 +1,31 @@
+/*alert("đây là trang web !");
+var name = prompt("moi nhap vao ten");
+console.log("Tên của bạn là : "+ name);  
+alert("chào mừng bạn "+ name +" đến với trang web ")
+*/
+/*function south (message){
+    const a = "abcdefghijklmnopqrstuvwxyz";
+    const b = "zxcvbnmasdfghjklqwertyuiop";
+    let macdinh = "";
+    for(let i=0;i< message.length;i++){
+        let char = message[i].toLowerCase();
+        if(a.includes(char)){
+            let index = a.indexOf(char);
+            macdinh+=b[index];
+
+        }
+        else{
+            macdinh+=char;
+        }
+    }
+    return macdinh;
+}
+let messagenhap = prompt("Mời nhập vào tin nhắn cần mã hoá");
+let kq = south(messagenhap);
+alert(`Tin nhắn được mã hoá là : ${kq}`);*/
+
+
+
+
+
+
